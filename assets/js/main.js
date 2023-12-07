fa3c0ea2e25d632d2af641e8d7cce03f6505616e
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2023-12-07 08:24:14 -0300

    INserção botão detalhes e preparo api detalhes

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,12 +10,12 @@ function convertPokemonToLi(pokemon) {
         <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
-
             <div class="detail">
                 <ol class="types">
                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                <button class="dt">Details</button>
                 </ol>
-
+                
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
             </div>
